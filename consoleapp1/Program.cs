5e9804ab6e3ee9ad2541c19bba4c993dcbc586b6
Twilight <lbillingh@gmail.com>
2021-11-13 10:48:24 -0800

    Initial commit

---
+++ consoleapp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace consoleapp1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
