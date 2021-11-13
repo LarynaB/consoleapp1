5f444c54975c9c9804c1ae2a62b9be47b6550dfb
Twilight <lbillingh@gmail.com>
2021-11-13 13:56:48 -0800

    update c#

--- consoleapp1/Program.cs
+++ consoleapp1/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace consoleapp1
+namespace Syntax
 {
-    class MainClass
+    class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool doit = false;
+            string str = "Hello";
+
+            Console.WriteLine("Checking status...");
+
+            if (doit)
+            {
+                doit = false;
+                Console.WriteLine("set to false");
+            }
+
+            Console.ReadKey();
         }
     }
 }
